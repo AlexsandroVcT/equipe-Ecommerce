f47f7935d5b90e7c99a52805412f6b0e90eb7c9d
Alexsandro VcT <alexsandro.vct@outlook.com>
2022-10-28 02:12:46 -0300

    resolvendo problemas

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ window.addEventListener("scroll", function () {
 })
 
 // CARROSSEL PRODUTOS EM DESTAQUES
-$('.carrone').slick({
+$('.carrossel').slick({
   prevArrow: '.slick-prev-one',
   nextArrow: '.slick-next-one',
   autoplay: true,
@@ -55,54 +55,55 @@ $('.carrone').slick({
   ]
 });
 
-$('.carrtwo').slick({
-  prevArrow: '.slick-prev-two',
-  nextArrow: '.slick-next-two',
-  autoplay: true,
-  autoplaySpeed: 4000,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        infinite: true,
-      }
-    },
-    {
-      breakpoint: 947,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 500,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
+// BUGUE NO CARROSEL
+// $('.carrone').slick({
+//   prevArrow: '.slick-prev-one',
+//   nextArrow: '.slick-next-one',
+//   autoplay: true,
+//   autoplaySpeed: 4000,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 4,
+//   slidesToScroll: 1,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//         infinite: true,
+//       }
+//     },
+//     {
+//       breakpoint: 947,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 1
+//       }
+//     },
+//     {
+//       breakpoint: 424,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1
+//       }
+//     }
+//   ]
+// });
 
 
 /* OPEN PAGE LOGIN REGISTER */
